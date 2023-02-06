9501421254ace0db0589514d019b255577349bd0
Taha7 <taha.mohamed4213@gmail.com>
2023-02-06 18:12:34 +0200

    add models and migrations

---
+++ app/Models/Ingredient.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient extends Model
+{
+    use HasFactory;
+
+    public function openingStock()
+    {
+        return $this->hasOne(OpeningStock::class);
+    }
+
+    public function stock()
+    {
+        return $this->hasOne(stock::class);
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = ['name', 'is_available'];
+
+    public function ingredients()
+    {
+        return $this->belongsToMany(Ingredient::class, 'product_ingredients')->withPivot(['quantity']);
+    }
+}
